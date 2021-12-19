ba3b3f62ab0cd1d985646ff793dd94e606fd702f
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-19 14:06:20 +0900

    feat: railsにリクエストを送り新規登録する

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -9,22 +9,43 @@
     <input type="password" required placeholder="パスワード（確認用）" v-model="passwordConfirmation">
     <button>登録する</button>
   </form>
+  <div class="error">{{ error }}</div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       name: '',
       email: '',
       password: '',
-      passwordConfirmation: ''
+      passwordConfirmation: '',
+      error: null
     }
   },
   methods: {
     async signUp (){
+      this.error = null
+      try {
+        const res = await axios.post('http://localhost:3000/auth',{
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          passwordConfirmation: this.passwordConfirmation
+        })
+        if(!res) {
+          throw new Error("アカウントを登録できませんでした")
+        }
+        console.log({res})
+        return res
+      }catch (error){
+        console.log({error})
+        this.error = "アカウントを登録できませんでした"
+      }
       console.log(this.name,this.email)
     }
   }
 }
 </script>
+
