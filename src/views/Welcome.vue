7f5aedb8ed1909b0466d7c558388fb711d5345f9
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-19 10:07:35 +0900

    feat: ボタンを押すと、isEnabled属性を切り替え、trueになると任意の項目を表示する

---
+++ src/views/Welcome.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>
+    Welcomeページ
+  </h1>
+  <section>
+    <h2>{{title}}</h2>
+    <section>
+      <h3>{{subtitle}}</h3>
+      <p v-if=isEnabled>{{3 + 3}}</p>
+      <button v-on:click="toggle">トグルする</button>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title: "初めてのVueアプリ",
+      subtitle: "ようこそ",
+      isEnabled: false
+    }
+  },
+  methods: {
+    toggle(){
+      this.isEnabled = !this.isEnabled
+    }
+  }
+}
+</script>
+
