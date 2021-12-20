01f32083c14682798d4f45d33be1caf8e59edd3b
RyomaKaneko <banzai.nippon2030@gmail.com>
2021-12-20 15:55:11 +0900

    feat: railsからメッセージ一覧を取得

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -2,20 +2,48 @@
   <section>
     <div class="container">
       <Navbar/>
+      <ChatWindow :messages="messages"/>
     </div>
-    <h1>
-      チャットルームです
-    </h1>
   </section>
 
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
+import ChatWindow from "../components/ChatWindow";
+import axios from 'axios'
 export default {
-  components: {Navbar}
+  components: {ChatWindow, Navbar},
+  data(){
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    async getMessages (){
+      try {
+        const res = await axios.get('http://localhost:3000/messages',{
+          headers: {
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+       if(!res){
+         throw new Error("メッセージ一覧を取得できませんでした。")
+       }
+       this.messages = res.data
+      }catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  mounted() {
+    this.getMessages()
+  }
 }
 </script>
 
 <style scoped>
 </style>
+
